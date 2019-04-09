2d5a50f321a4c3408b8ffce9d0016c8a4cf7096d
Jason Lengstorf <jason@lengstorf.com>
2019-04-08 19:09:34 -0700

    fix: lazy load routes

--- src/components/app/router.js
+++ src/components/app/router.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Router } from '@reach/router';
+import Loading from '../loading/loading';
 
-import Home from '../../pages/home';
-import Add from '../../pages/add';
-import Login from '../../pages/login';
-import User from '../../pages/user';
-import Dashboard from '../../pages/dashboard';
+const Home = lazy(() =>
+  import('../../pages/home' /* webpackChunkName: "home" */)
+);
+const Add = lazy(() => import('../../pages/add' /* webpackChunkName: "add" */));
+const Login = lazy(() =>
+  import('../../pages/login' /* webpackChunkName: "login" */)
+);
+const User = lazy(() =>
+  import('../../pages/user' /* webpackChunkName: "user" */)
+);
+const Dashboard = lazy(() =>
+  import('../../pages/dashboard' /* webpackChunkName: "dashboard" */)
+);
 
 const AppRouter = () => (
-  <Router>
-    <Home path="/" />
-    <Add path="/add" />
-    <Login path="/login" />
-    <User path="/:username" />
-    <Dashboard path="/dashboard" />
-  </Router>
+  <Suspense fallback={<Loading />}>
+    <Router>
+      <Home path="/" />
+      <Add path="/add" />
+      <Login path="/login" />
+      <User path="/:username" />
+      <Dashboard path="/dashboard" />
+    </Router>
+  </Suspense>
 );
 
 export default AppRouter;
