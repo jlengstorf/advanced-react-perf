73ef185c6952c51e50623606ac8dc79401aad727
Jason Lengstorf <jason@lengstorf.com>
2019-04-08 21:58:41 -0700

    fix: stop chart from blocking dashboard render

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { useUser } from '../../context/user';
 import { useShamecaps } from '../../context/shamecaps';
 import Layout from '../layout/layout';
-import Loading from '../loading/loading';
 import Shamelist from '../shamelist/shamelist';
-import { useAnalytics } from '../../context/analytics';
 import Chart from './chart';
 
 import './dashboard.scss';
 
 const Dashboard = () => {
-  const { loading, metrics } = useAnalytics();
   const { user } = useUser();
   const {
     shamecaps,
@@ -22,16 +19,14 @@ const Dashboard = () => {
     user: user.name
   });
 
-  return loading ? (
-    <Loading />
-  ) : (
+  return (
     <>
       <Layout>
         <h1 className="dashboard-heading">Your Account</h1>
         <p>Here’s how people feel about your shamecaps:</p>
       </Layout>
       <div className="chart">
-        <Chart metrics={metrics} />
+        <Chart />
       </div>
       <Layout>
         <Shamelist
